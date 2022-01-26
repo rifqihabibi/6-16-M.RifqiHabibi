05c9d23e0b804fad1ce41ee4a56ea157de953a52
unknown <3103120129@student.smktelkom-pwt.sch.id>
2022-01-26 10:23:32 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+</head>
+<body>
+    <h1>ABOUT RIFKI</h1>
+    <h3> <?php echo $nama; ?> </h3>
+    <p> <?php echo $email; ?> </p>
+    <img src="images/Rifki.jpg" alt="Rifki" width="200px">    
+</body>
+</html>
